a282135bb28809703b584f69ab1f7df225f00c25
Danielle <daniellejjhouser@gmail.com>
2024-10-15 19:14:44 +0200

    Update index.js
    humidity

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ function displayTemperature(response) {
   let temperature = Math.round(response.data.temperature.current);
   let cityElement = document.querySelector("#current-city");
   let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
   
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = '${response.data.temperature.humidity}%';
   temperatureElement.innerHTML = temperature;
 }
 function search(event) {
